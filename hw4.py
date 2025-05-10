41c9651ce1c104b6a620d6ff292650bb53cece5b
Yusif Panahov <yusifpanahov@gmail.com>
2025-05-10 04:18:07 -0700

    init

--- hw4.py
+++ hw4.py
@@ -3,7 +3,7 @@ import os
 
 FAYL = "expenses.json"
 
-def yüklə():
+def yukle():
     if os.path.exists(FAYL):
         with open(FAYL, "r") as f:
             return json.load(f)
@@ -13,29 +13,29 @@ def yadda_saxla(data):
     with open(FAYL, "w") as f:
         json.dump(data, f, indent=2)
 
-def xərc_əlavə_et():
-    başlıq = input("Başliq: ")
+def xerc_elave_et():
+    basliq = input("Basliq: ")
     kateqoriya = input("Kateqoriya: ")
-    məbləğ = float(input("Mebleg: "))
-    data = yüklə()
-    data.append({"başliq": başlıq, "kateqoriya": kateqoriya, "mebleg": məbləğ})
+    mebleg = float(input("Mebleg: "))
+    data = yukle()
+    data.append({"basliq": basliq, "kateqoriya": kateqoriya, "mebleg": mebleg})
     yadda_saxla(data)
-    print("Elavə olundu.")
+    print("Elave olundu.")
 
-def xərc_sil():
-    başlıq = input("Silinecek başliq: ")
-    data = yüklə()
-    data = [x for x in data if x["başliq"] != başliq]
+def xerc_sil():
+    basliq = input("Silinecek basliq: ")
+    data = yukle()
+    data = [x for x in data if x["basliq"] != basliq]
     yadda_saxla(data)
     print("Silindi")
 
-def xərc_yenilə():
-    başlıq = input("Yenilenecek başliq: ")
-    yeni_məbləğ = float(input("Yeni mebleğ: "))
-    data = yüklə()
+def xerc_yenile():
+    basliq = input("Yenilenecek basliq: ")
+    yeni_mebleg = float(input("Yeni mebleg: "))
+    data = yukle()
     for x in data:
-        if x["başliq"] == başlıq:
-            x["mebleğ"] = yeni_məbləğ
+        if x["basliq"] == basliq:
+            x["mebleg"] = yeni_mebleg
     yadda_saxla(data)
     print("Yenilendi.")
 
@@ -44,16 +44,16 @@ def menyu():
         print("\n1. Xerc elave et")
         print("2. Xerc sil")
         print("3. Xerc yenile")
-        print("4. Çix")
+        print("4. cix")
 
         secim = input("Secim: ")
 
         if secim == "1":
-            xərc_əlavə_et()
+            xerc_elave_et()
         elif secim == "2":
-            xərc_sil()
+            xerc_sil()
         elif secim == "3":
-            xərc_yenilə()
+            xerc_yenile()
         elif secim == "4":
             print("Sag ol ")
             break
