0d4d7f087dadf8d3d1a9a318eb41d09ff6836a56
Yusif Panahov <yusifpanahov@gmail.com>
2025-05-10 03:23:12 -0700

    init

--- hw4.py
+++ hw4.py
@@ -3,77 +3,61 @@ import os
 
 FAYL = "expenses.json"
 
-
 def yüklə():
     if os.path.exists(FAYL):
         with open(FAYL, "r") as f:
             return json.load(f)
-
-
-return []
-
+    return []
 
 def yadda_saxla(data):
     with open(FAYL, "w") as f:
         json.dump(data, f, indent=2)
 
-
 def xərc_əlavə_et():
-
-
-başlıq = input("Başlıq: ")
-kateqoriya = input("Kateqoriya: ")
-məbləğ = float(input("Məbləğ: "))
-data = yüklə()
-data.append({"başlıq": başlıq, "kateqoriya": kateqoriya, "məbləğ": məbləğ})
-yadda_saxla(data)
-print("Əlavə olundu.")
-
+    başlıq = input("Başliq: ")
+    kateqoriya = input("Kateqoriya: ")
+    məbləğ = float(input("Mebleg: "))
+    data = yüklə()
+    data.append({"başliq": başlıq, "kateqoriya": kateqoriya, "mebleg": məbləğ})
+    yadda_saxla(data)
+    print("Elavə olundu.")
 
 def xərc_sil():
-
-
-başlıq = input("Silinəcək başlıq: ")
-data = yüklə()
-data = [x for x in data if x["başlıq"] != başlıq]
-yadda_saxla(data)
-print("Silindi.")
-
+    başlıq = input("Silinecek başliq: ")
+    data = yüklə()
+    data = [x for x in data if x["başliq"] != başliq]
+    yadda_saxla(data)
+    print("Silindi")
 
 def xərc_yenilə():
-
-
-başlıq = input("Yenilənəcək başlıq: ")
-yeni_məbləğ = float(input("Yeni məbləğ: "))
-data = yüklə()
-for x in data:
-if x["başlıq"] == başlıq:
-x["məbləğ"] = yeni_məbləğ
-yadda_saxla(data)
-print("Yeniləndi.")
-
+    başlıq = input("Yenilenecek başliq: ")
+    yeni_məbləğ = float(input("Yeni mebleğ: "))
+    data = yüklə()
+    for x in data:
+        if x["başliq"] == başlıq:
+            x["mebleğ"] = yeni_məbləğ
+    yadda_saxla(data)
+    print("Yenilendi.")
 
 def menyu():
+    while True:
+        print("\n1. Xerc elave et")
+        print("2. Xerc sil")
+        print("3. Xerc yenile")
+        print("4. Çix")
 
+        secim = input("Secim: ")
 
-while True:
-print("\n1. Xərc əlavə et")
-print("2. Xərc sil")
-print("3. Xərc yenilə")
-print("4. Çıx")
-
-secim = input("Seçim: ")
-
-if secim == "1":
-xərc_əlavə_et()
-elif secim == "2":
-xərc_sil()
-elif secim == "3":
-xərc_yenilə()
-elif secim == "4":
-print("Sağ ol brat!")
-break
-else:
-print("Yanlış seçim.")
+        if secim == "1":
+            xərc_əlavə_et()
+        elif secim == "2":
+            xərc_sil()
+        elif secim == "3":
+            xərc_yenilə()
+        elif secim == "4":
+            print("Sag ol ")
+            break
+        else:
+            print("Yanlis secim.")
 
 menyu()
